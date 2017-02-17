2b6c8f16d7857387b70a04d6c8d73c89b657aebb
mzyndul <michalzyndul@gmail.com>
2017-02-17 15:11:13 +0100

    Add .gitignore and refactor Environment.py
    remove editor files from the project and add them to gitignore
    small refactor of Environment.py file to look more pythonic way

--- src/bin/Environment.py
+++ src/bin/Environment.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on 30-Jan-2015
 
 @author: Asawari.Vaidya
-'''
+"""
 
 import certifi
 from urllib3.connectionpool import HTTPSConnectionPool
@@ -10,20 +10,12 @@ from urllib3.util.timeout import Timeout
 
 
 class Environment:
-    '''
-    classdocs
-    '''
-    
-    def __init__(self, host_url, max_connections, pool_enable, 
-                 connection_timeout, read_timeout):
-        '''
-        Constructor
-        '''
-        if connection_timeout is None:
-            connection_timeout = 30
-        if read_timeout is None:
-            read_timeout = 30
-        
+
+    ENV_TEST = None
+    ENV_PROD = None
+
+    def __init__(self, host_url, max_connections=10, pool_enable=True,
+                 connection_timeout=30, read_timeout=30):
         
         self._host_url = host_url
         self._pool = HTTPSConnectionPool(host_url, 
@@ -35,18 +27,17 @@ class Environment:
                                          retries=False,
                                          cert_reqs='CERT_REQUIRED',
                                          ca_certs=certifi.where())
-        
-    @classmethod
-    def _get_env(self, params_env):
-        self._params_env = params_env 
-        if self._params_env == 'TEST':
-            return environment_url['TEST']
-        if self._params_env == 'LIVE':
-            return environment_url['LIVE']
 
-# Class Object      
-environment_url = dict()
-environment_url['TEST'] = Environment('api.test.paysafe.com', 10, True, 
-                                      connection_timeout=30, read_timeout=30)
-environment_url['LIVE'] = Environment('api.paysafe.com', 10, True, 
-                                      connection_timeout=30, read_timeout=30)
+    @classmethod
+    def _get_env(cls, params_env):
+        if params_env == 'TEST':
+            if not Environment.ENV_TEST:
+                # TODO both url addresses should be in some kind of config file, and not here
+                Environment.ENV_TEST = Environment('api.test.paysafe.com')
+            return Environment.ENV_TEST
+        elif params_env == 'LIVE':
+            if not Environment.ENV_TEST:
+                Environment.ENV_PROD = Environment('api.paysafe.com')
+            return Environment.ENV_PROD
+        # TODO in case user enter wrong env, this should raise some nice error.
+        raise Exception('env accepts only TEST or LIFE as valid parameters')
